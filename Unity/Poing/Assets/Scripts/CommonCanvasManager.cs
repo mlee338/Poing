3c45940ba332cf5f8ae4605f5cebf78f41f35500
William Tung-Mao Wu <william.tm.wu@gmail.com>
2015-11-28 11:36:19 +1000

    skeleton

---
+++ Unity/Poing/Assets/Scripts/CommonCanvasManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommonCanvasManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Poing/Assets/Scripts/PlayerCanvasManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCanvasManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
